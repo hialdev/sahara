297dc32ed41115ac698f62f3eccfa5ddb10f5fbe
hialdev <al.developer12@gmail.com>
2021-07-21 17:54:14 +0700

    homepage first deploy

---
+++ src/js/style.js
@@ -0,0 +1,51 @@
+var header = $('.header');
+
+//------ Header
+var header = $('.header');
+var textDropdown = $('nav.menu ul li.dropdown ul li a');
+var textMenu = $('nav.menu ul li a');
+var menuBox = $('.menu-box');
+
+var width = $(window).width();
+
+$(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        
+        if (scroll >= 185 || header.hasClass('static')) {
+            header.addClass('active');
+        } else if(scroll < 185) {
+            header.removeClass('active');
+        }
+    });
+});
+
+//menu-toggle
+$('.menu-toggle').click(function(){
+    $('.menu-box').toggleClass('slide-menu');
+});
+
+//------------------------ 
+// Slider
+//------------------------
+
+//News
+$('.news-box').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    nav:false,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            loop:false
+        }
+    }
+})
